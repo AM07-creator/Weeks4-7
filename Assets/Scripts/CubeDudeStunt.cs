caa32f67f8ca94bec4b5f7572c71c791998ba7ab
AM07-creator <mancaa@sheridancollege.ca>
2026-02-15 21:36:34 -0500

    Button now successfully spawns the CudeDude

--- Assets/Scripts/CubeDudeStunt.cs
+++ Assets/Scripts/CubeDudeStunt.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CubeDudeStunt : MonoBehaviour
 {
-    // This script will be used to controle the button's effects. When pressed, it will make the CubeDude Prefab respawn at a random screenspace position and EulerAngle
+    // This script will be used to controle the button's effects. When pressed, it will make the CubeDude Prefab respawn at a random screenspace position and EulerAngle. The Prefab will automatically disappear after a few seconds via timer
 
     public GameObject cubeDudePrefab;
 
@@ -16,7 +16,11 @@ public class CubeDudeStunt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 spawnPos = Random.insideUnitCircle * 3;
-        Instantiate(cubeDudePrefab, transform.position, transform.rotation);
+
     }
+    public void SpawnCubeDude()
+    {
+		Vector2 spawnPos = Random.insideUnitCircle * 3;
+		Instantiate(cubeDudePrefab, transform.position, transform.rotation);
+	}
 }
