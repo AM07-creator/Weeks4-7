2a5642c312f93431eb27bf3da864fa1f5fea5ad1
AM07-creator <mancaa@sheridancollege.ca>
2026-02-16 00:01:54 -0500

    CubeDude now destroys himself if pressed again after initially spawning, and respawns at a random transform and rotation in the unit circle

--- Assets/Scripts/CubeDudeStunt.cs
+++ Assets/Scripts/CubeDudeStunt.cs
@@ -3,36 +3,39 @@ using UnityEngine.UI;
 
 public class CubeDudeStunt : MonoBehaviour
 {
-    // This script will be used to controle the button's effects. When pressed, it will make the CubeDude Prefab respawn at a random position in the unit circle and random EulerAngle. The Prefab will automatically disappear after a few seconds via timer
+	public float speed = 5.0f;
+	public GameObject cubeDudePrefab;
+	public Transform spawnPoint;
 
-    public float speed = 0.01f;
-    public GameObject cubeDudePrefab;
-    public Transform spawnPoint;
+	private GameObject currentInstance;
 
-    Vector2 bottomLeft;
-    Vector2 topRight;
+	Vector2 bottomLeft;
+	Vector2 topRight;
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
-    {
-        //world space for CubeDude to spawn/respawn in
-        bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
-        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-    }
+	{
+		// World space boundaries for spawning CubeDude
+		bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
+		topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	public void SpawnCubeDude()
+	{
+		// Destroy the previous instance if it exists (only happens if the button has been pressed once already, effectively deleting CubeDude and respawning him somewhere new with a new random rotation
+		if (currentInstance != null)
+		{
+			Destroy(currentInstance);
+		}
 
-    }
-    public void SpawnCubeDude()
-    {
-		// local variable for rotation
+		// Generate random rotation
 		float randomZ = Random.Range(0f, 360f);
-		// pick a random z rotation
 		Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
-        //instantiate the prefab transform pos and random z rotation
-        Vector2 spawnPos = Random.insideUnitCircle * 5;
-		Instantiate(cubeDudePrefab, spawnPos, randomRotation);
+
+		// Generate random position in a unit circle 
+		Vector2 randomCirclePos = Random.insideUnitCircle * 5;
+		Vector3 spawnPos = new Vector3(randomCirclePos.x, randomCirclePos.y, 0) + spawnPoint.position;
+
+		// Instantiate the new prefab
+		currentInstance = Instantiate(cubeDudePrefab, spawnPos, randomRotation);
 	}
 }
