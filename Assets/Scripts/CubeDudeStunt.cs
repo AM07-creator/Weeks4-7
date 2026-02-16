25fe7bd8e8183a72080782c40fffe430c0e8f6cc
AM07-creator <mancaa@sheridancollege.ca>
2026-02-16 15:43:27 -0500

    A timer now causes the button to be pressed automatically after a few seconds.  I have all requirements met now for the assignment

--- Assets/Scripts/CubeDudeStunt.cs
+++ Assets/Scripts/CubeDudeStunt.cs
@@ -5,14 +5,13 @@ using UnityEngine.UI;
 
 public class CubeDudeStunt : MonoBehaviour
 {
+//This script controls where CubeDude can spawn and respawn, as well as how he spawns and respawns, and automatically clicks the button after a timer ends
 	public float speed = 5.0f;
 	public GameObject cubeDudePrefab;
 	public Transform spawnPoint;
-	public float t;
+	public float t = 0f;
 	private GameObject currentInstance;
-	//public Transform start;
-	//public Transform end;
-	public AnimationCurve curve;
+	public Button targetButton;
 
 	Vector2 bottomLeft;
 	Vector2 topRight;
@@ -25,16 +24,18 @@ public class CubeDudeStunt : MonoBehaviour
 	}
 	void Update()
 	{
-		//t += Time.deltaTime;
-		//if (t > 3)
-		//{
-		//	t = 0;
-		//}
-		//transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+		t += Time.deltaTime;
+		if (t > 3)
+		{
+		// Automatically clicks the button after 3 second timer ends
+			targetButton.onClick.Invoke();
+			t = 0;
+		}
+
 	}
 	public void SpawnCubeDude()
 	{
-		// Destroy the previous instance if it exists (only happens if the button has been pressed once already, effectively deleting CubeDude and respawning him somewhere new with a new random rotation
+		// Destroy the previous CubeDude if it exists (only happens if the button has been pressed once already, effectively deleting CubeDude and respawning him somewhere new with a new random rotation
 		if (currentInstance != null)
 		{
 			Destroy(currentInstance);
