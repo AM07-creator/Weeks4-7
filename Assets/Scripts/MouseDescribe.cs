107d5aab3a27da25fd4db4863f39673b1d8ca1a7
AM07-creator <mancaa@sheridancollege.ca>
2026-06-08 17:55:55 -0400

    Challenge remains incomplete, though near completion with some help

--- Assets/Scripts/MouseDescribe.cs
+++ Assets/Scripts/MouseDescribe.cs
@@ -4,9 +4,10 @@ using UnityEngine.InputSystem;
 public class MouseDescribe : MonoBehaviour
 {
     public Camera gameCamera;
-    public SpriteRenderer sprite;
+    public SpriteRenderer sprite1;
 	public SpriteRenderer sprite2;
-    //bool isRevealed = false;
+	bool isOver = false;
+    public GameObject text;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,17 +23,19 @@ public class MouseDescribe : MonoBehaviour
 		Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(mousePosition);
 		worldMousePosition.z = 0f;
 
-        if(sprite.bounds.Contains(worldMousePosition))
+        if(sprite1.bounds.Contains(worldMousePosition))
         {
-            Debug.Log("IsOver");
+            Debug.Log("IsOverCassette");
+            isOver = true;
         }
 		if (sprite2.bounds.Contains(worldMousePosition))
 		{
-			Debug.Log("IsOver");
+			Debug.Log("IsOverCar");
+			isOver = true;
 		}
+        if(isOver == true)
+        {
+            //text;
+        }
 	}
-    //public void OnHoverReveal()
-    //{
-    //    bool isRevealed = true;
-    //}
 }
